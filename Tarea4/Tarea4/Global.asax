23e9fde3c460483e6b1e081159ea60780fef5091
Carmen Sanchez <carmenmasato@gmail.com>
2020-02-23 22:19:40 -0400

    -Commit prueba

---
+++ Tarea4/Tarea4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tarea4.MvcApplication" Language="C#" %>
---
+++ Tarea4/Tarea4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tarea4.Startup))]
+namespace Tarea4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
