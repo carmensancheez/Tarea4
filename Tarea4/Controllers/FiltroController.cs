3dc4234dfcbfea934c2179c3a55c627516e35edb
Yatzary <nicolesantanam@hotmail.com>
2020-02-24 11:44:16 -0400

    Se agregó el módulo filtro de contenidos, controlador y vistas

---
+++ Tarea4/Controllers/FiltroController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tarea4.Controllers
+{
+    public class FiltroController : Controller
+    {
+        // GET: Filtro
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Noticias()
+        {
+            return View();
+        }
+        public ActionResult Musica()
+        {
+            return View();
+        }
+        public ActionResult Peliculas()
+        {
+            return View();
+        }
+    }
+}
